2207bdefe98b2d7431ff2084c5faa8480f5015bb
Gauransh Baunthiyal <gauranshbaunthiyal@gmail.com>
2026-07-23 23:02:09 +0530

    Insert an element in an array

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        //Input the size of the array
+        System.out.println("Enter the size of array");
+        int n= sc.nextInt();
+        //Input the array
+        System.out.println("Enter the array");
+        int arr[]= new int[n+1];
+        //Input the position of the element to be inserted
+        for(int i=0; i<n; i++){
+            arr[i]= sc.nextInt();
+        }
+        System.out.println("Enter the position where the element is to be inserted");
+        int pos= sc.nextInt();
+        //Input the element to be inserted
+        System.out.println("Enter the element to be inserted");
+        int x= sc.nextInt();
+        //Shift elements to the right
+        for(int i=n; i>=pos; i--){
+            arr[i]= arr[i-1];
+        }
+        //Insert the new element
+        arr[pos-1]= x;
+        //Printing the array
+        System.out.println("The array is: ");
+        for(int i=0; i<=n; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
