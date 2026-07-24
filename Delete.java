f3ce1e2b519747b86a9a2d603a015c0299cd9913
Gauransh Baunthiyal <gauranshbaunthiyal@gmail.com>
2026-07-24 14:22:27 +0530

    Delete an element from an array

--- Delete.java
+++ Delete.java
@@ -1,35 +1,31 @@
 import java.util.*;
 
-public class Main{
+public class Delete {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
-        //Input the size of the array
+        //Input the size of array
         System.out.println("Enter the size of array");
         int n= sc.nextInt();
         //Input the array
         System.out.println("Enter the array");
-        int arr[]= new int[n+1];
+        int arr[]= new int[n];
         for(int i=0; i<n; i++){
             arr[i]= sc.nextInt();
         }
-        //Input the position of the element to be inserted
-        System.out.println("Enter the position where the element is to be inserted");
+        //Input the position of the element to be deleted
+        System.out.println("Enter the position of the element to be deleted");
         int pos= sc.nextInt();
-        //Input the element to be inserted
-        System.out.println("Enter the element to be inserted");
-        int x= sc.nextInt();
-        //Shift elements to the right
-        for(int i=n; i>=pos; i--){
-            arr[i]= arr[i-1];
+        //Shift the elements to the left
+        for(int i= pos-1; i<n-1; i++){
+            arr[i]= arr[i+1];
         }
-        //Insert the new element
-        arr[pos-1]= x;
         //Printing the array
         System.out.println("The array is: ");
-        for(int i=0; i<=n; i++){
+        for(int i=0; i<n-1; i++){
             System.out.print(arr[i]+ " ");
         }
         System.out.println();
         sc.close();
+        }
     }
-}
+
