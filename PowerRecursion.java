757a2e34d0839533803012adc0ad4fd282bef078
Gauransh Baunthiyal <gauranshbaunthiyal@gmail.com>
2026-07-30 13:39:14 +0530

    Calculate power using recursion

---
+++ PowerRecursion.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class PowerRecursion {
+    //Function to find a raise to the power b
+    public static int power(int a, int b){
+        //Base case
+        if( b == 0 ){
+            return 1;
+        }
+        //Recursive case
+        return a * power(a, b-1);
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // Input a 
+        System.out.println("Enter the value of a");
+        int a= sc.nextInt();
+        //Input b
+        System.out.println("Enter the value of b");
+        int b= sc.nextInt();
+        System.out.println(a + " Raised to the power " + b + " is: " + power(a,b));
+        System.out.println();
+        sc.close();
+    }
+}
